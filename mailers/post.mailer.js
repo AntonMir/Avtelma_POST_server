a2efa7180846c5b513e831e4d5f275c77735a7ba
AntonMir <toshik124@rambler.ru>
2022-05-09 00:00:43 +0300

    filtrated req.body: from Avtelma or from SmartIns...

--- mailers/post.mailer.js
+++ mailers/post.mailer.js
@@ -4,8 +4,11 @@ function sendEmail(sender, senderPass, receiver, userData) {
     // create reusable transporter object using the default SMTP transport
     const { name, email, phone, company, question, appName } = userData
 
-    // base mail message
-    const mailMessage = `
+    let mailMessage
+
+    if (!phone || !company) {
+        // base mail message
+        mailMessage = `
         <hr>
         <h4>Question sent from:</h4>
         <ul>  
@@ -15,8 +18,6 @@ function sendEmail(sender, senderPass, receiver, userData) {
         <ul>  
             <li>Name: ${name}</li>
             <li>Email: ${email}</li>
-            <li>Phone: ${phone}</li>
-            <li>Company name: ${company}</li>
         </ul>
         <h4>Question:</h4>
         <ul>  
@@ -24,6 +25,31 @@ function sendEmail(sender, senderPass, receiver, userData) {
         </ul>
         <hr>
     `
+    } else {
+        // base mail message
+        mailMessage = `
+            <hr>
+            <h4>Question sent from:</h4>
+            <ul>  
+                <li>${appName}</li>
+            </ul>
+            <h4>Contact details:</h4>
+            <ul>  
+                <li>Name: ${name}</li>
+                <li>Email: ${email}</li>
+                <li>Phone: ${phone}</li>
+                <li>Company name: ${company}</li>
+            </ul>
+            <h4>Question:</h4>
+            <ul>  
+                <li>${question}</li>
+            </ul>
+            <hr>
+        `
+    }
+
+
+
     // open smtp channel
     const smtpTransport = nodemailer.createTransport({
         host: 'mail.nic.ru',
